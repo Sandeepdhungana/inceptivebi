1b916dc93c6b6d0a6a8803b34588c0f4e3c61428
Sandeep Dhungana <sandeepdhungana10@gmail.com>
2024-09-02 19:38:40 +0530

    added url

--- src/url.js
+++ src/url.js
@@ -1,3 +1,5 @@
 // export const url = "http://127.0.0.1:5050/api"
 
-export const url = "http://44.204.61.35:5050/api"
+// export const url = "http://44.204.61.35:5050/api"
+
+export const url = "https://inceptivebi.com/api"
